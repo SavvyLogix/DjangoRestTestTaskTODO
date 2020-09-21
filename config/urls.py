d77188bfee272d92d41a20eff0908246d7da4520
SavvyLogix <danylov.alex02@gmail.com>
2020-09-21 17:05:48 +0300

    Initial commit

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
