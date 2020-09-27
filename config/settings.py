d8d34023307c8513e7ca93860569217d9ea4f687
SavvyLogix <danylov.alex02@gmail.com>
2020-09-27 17:33:09 +0300

    to_do application models were created

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
+    'to_do',
 ]
 
 MIDDLEWARE = [
--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=100)
+
+    def __str__(self):
+        return 'Board {}'.format(self.name)
+
+class TodoList(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=100)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    board = models.ForeignKey(Board, verbose_name='Board', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return 'TodoList {}, created at{}'.format(self.title, self.created)
