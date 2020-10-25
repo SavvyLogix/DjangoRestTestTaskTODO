8badf95eebcba542afc350c3b058579b9e387433
SavvyLogix <danylov.alex02@gmail.com>
2020-10-25 20:21:54 +0200

    added REST API Board model

--- to_do/models.py
+++ to_do/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Board(models.Model):
     name = models.CharField(verbose_name='Name', max_length=100)
 
     def __str__(self):
         return 'Board {}'.format(self.name)
+
 
 class TodoList(models.Model):
     title = models.CharField(verbose_name='Title', max_length=100)
